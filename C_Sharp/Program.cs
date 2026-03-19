43a4b0558848767ed875cf262b97f06a3d28b90a
ms <shimash.ca@gmail.com>
2026-03-19 13:49:58 -0400

    Variables

--- C_Sharp/Program.cs
+++ C_Sharp/Program.cs
@@ -14,6 +14,34 @@ namespace C_Sharp
             //Printing Hello World to the console
             Console.WriteLine("Hello World!");
 
+            //variables
+
+            //integer
+            int age = 22;
+            Console.WriteLine(age);
+
+
+            //long
+            long bigNumber = 9000000000L;
+            Console.WriteLine(bigNumber);
+
+            //double
+            double negative = -55.8D;
+            Console.WriteLine(negative);
+
+            //float
+            float precision = 5.00001F;
+            Console.WriteLine(precision);
+
+
+            //methonds of initializing variables
+            //1
+            //int x;
+            //int y;
+            //int z;
+
+            //2
+            int x, y, z;
         }
     }
 }
