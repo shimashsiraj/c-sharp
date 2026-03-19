62f87236d37207f860629f73347a204b583e7ad4
ms <shimash.ca@gmail.com>
2026-03-19 14:07:09 -0400

    String Conversion

--- C_Sharp/Program.cs
+++ C_Sharp/Program.cs
@@ -17,21 +17,21 @@ namespace C_Sharp
             //variables
 
             //integer
-            int age = 22;
-            Console.WriteLine(age);
+            //int age = 22;
+            //Console.WriteLine(age);
 
 
-            //long
-            long bigNumber = 9000000000L;
-            Console.WriteLine(bigNumber);
+            ////long
+            //long bigNumber = 9000000000L;
+            //Console.WriteLine(bigNumber);
 
-            //double
-            double negative = -55.8D;
-            Console.WriteLine(negative);
+            ////double
+            //double negative = -55.8D;
+            //Console.WriteLine(negative);
 
-            //float
-            float precision = 5.00001F;
-            Console.WriteLine(precision);
+            ////float
+            //float precision = 5.00001F;
+            //Console.WriteLine(precision);
 
 
             //methonds of initializing variables
@@ -42,6 +42,29 @@ namespace C_Sharp
 
             //2
             int x, y, z;
+
+            //string conversion
+
+            //integer
+            string textAge = "22";
+            int age = Convert.ToInt32(textAge);
+            Console.WriteLine(age);
+
+
+            //long
+            string textBigNumber = "9000000000";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
+
+            //double
+            string textNegative = "-55.8";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+
+            //float
+            string textPrecision = "5.00001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
         }
     }
 }
